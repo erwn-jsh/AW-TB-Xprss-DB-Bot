0c24654c891a259abc8d42b5466185d37cb2092d
user <joshmanuel2016@gmail.com>
2023-01-23 16:07:18 -0500

    fix(models): make model for draw

--- models/draw.js
+++ models/draw.js
@@ -1,2 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema; // change variable name
+
+const DrawSchema = new Schema(
+  {
+    dateCreated: {
+      type: Date,
+      default: Date.now,
+    },
+    drawNo: {
+      type: Int32,
+    },
+    minimumCRS: {
+      type: Double,
+    },
+    dateOfDraw: {
+      type: Date,
+    },
+    noOfInvitations: {
+      type: Double,
+    },
+  },
+  {
+    collections: "draws",
+  }
+);
+
+const User = mongoose.model("Draw", DrawSchema);
+module.exports = User;
