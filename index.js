64aae1b20ade1d07cf3e0d0f350c94541760a962
user <joshmanuel2016@gmail.com>
2023-01-23 16:21:34 -0500

    style(index.js): format index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+// THIRD PARTY MODULES
+//////////////////////////////////////////////////////////////////////////////////////////
+require("dotenv").config();
+
 const PORT = 8000;
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 
+// Initialize app with express
 const app = express();
+console.log("App Started...");
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// CONFIGURATIONS
+//////////////////////////////////////////////////////////////////////////////////////////
+// DB Config
+// const DB = "mongodb+srv://santiago:OCIImJVgF3Jb2Ii0@mongodbserver.p6m24.mongodb.net/LEARNT?retryWrites=true&w=majority";
+// Connect to Mongo
+// mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true });
+// let mongoDB = mongoose.connection;
+// mongoDB.on("error", console.error.bind(console, "DB Connection Error: "));
+// mongoDB.once("open", () => {
+//   console.log("Connected to MongoDB...");
+// });
 
 const url =
   "https://www.canadavisa.com/express-entry-invitations-to-apply-issued.html";
@@ -34,17 +54,26 @@ axios(url)
     data = data.filter((data) => data.drawNo > 236);
     console.log(data);
     //foreach add make axios request to mongo to post
-
-    //TODO: make architecture diagram
-    //TODO: make 2 express servers
-    //TODO: web scraper run locally and run twitter
-    //TODO: make mongodb
-    //TODO: foreach add make axios request to mongo to post
-    //TODO: link with Twitter bot
-    //TODO: other to handle display of data; REACT?
-    //TODO: make routes to show webpage
-    //TODO: add chart
-    //TODO: deploy
   })
-  .catch((err) => console.log(err));
+  .catch((err) => () => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
+//TODO: make architecture diagram
+//TODO: make 2 express servers
+//TODO: web scraper run locally and run twitter
+//TODO: make mongodb
+//TODO: foreach add make axios request to mongo to post
+//TODO: link with Twitter bot
+//TODO: other to handle display of data; REACT?
+//TODO: make routes to show webpage
+//TODO: add chart
+//TODO: deploy
